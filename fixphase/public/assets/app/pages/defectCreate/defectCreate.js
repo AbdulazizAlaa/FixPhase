f05492fb394b77fcf2aaa443b5ec469329acc3e0
Aya Khaled <aya_94_a2@hotmail.com>
2015-05-17 04:33:32 +0200

    Modified defectCreate folder

--- fixphase/public/assets/app/pages/defectCreate/defectCreate.js
+++ fixphase/public/assets/app/pages/defectCreate/defectCreate.js
@@ -1,3 +1,77 @@
-/**
- * Created by karim on 4/27/15.
- */
+define(
+    [
+        "text!pages/defectCreate/defectCreate.html!strip",
+        "page",
+        'jquery',
+        "css!pages/defectCreate/defectCreate.css",
+        "css!assets/lib/bootstrap/css/bootstrap.min.css"
+    ],
+    function (html, Page, $) {
+
+        return new Page({
+
+            //a must have property where content are setup
+            makeContent: function () {
+                var content = $(($.parseHTML($.trim(html)))[0]);
+
+                var defectForm = content.find('#create-defect-form');
+
+                var headline = content.find('#headline');
+                var releaseName = content.find('#releasename');
+                var releaseVersion = content.find('#releaseversion');
+                var severity = content.find('#severity-role');
+                var priority = content.find('#priority-role');
+                var product = content.find('#product-role');
+                var defectDesc = content.find('#defect-desc');
+
+                var submitDefect = content.find('#submit-defect');
+
+
+                defectForm.submit(function (e) {
+                    e.preventDefault();
+
+                    var defectData = {
+                        'headline':headline.val(),
+                        'releasename':releaseName.val(),
+                        'releaseversion': releaseVersion.val(),
+                        'severity':content.find('#severity-role option:selected').text(),
+                        'priority':content.find('#priority-role option:selected').text(),
+                        'product':content.find('#product-role option:selected').text(),
+                        'defecrDesc':defectDesc.val()
+                    };
+
+                    $.ajax({
+                        type        : 'POST',
+                        url         : '/defects',
+                        data        : defectData,
+                        dataType    : 'json',
+                        encode          : true
+                    }).done(function () {
+                        self.goToURL("/projects/pid/defects/1");
+                    });
+
+
+                    e.preventDefault();
+                });
+
+                return content;
+            },
+
+            //all url observe goes here
+            observeURLS: function () {
+
+            },
+
+            //called when the page is in view
+            //you can fetch data and show them here on startup if you want
+            start: function () {
+
+            },
+
+            exit: function(){
+
+            }
+        });
+
+    }
+);
